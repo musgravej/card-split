83c976537da69d6ab70de0530fe08c4193c5358c
jim <jim.musgrave@gmail.com>
2018-12-05 12:37:25 -0600

    updated with cover page option, prompt

--- envelope_card_split.py
+++ envelope_card_split.py
@@ -1,11 +1,13 @@
 import PyPDF2
+import time
 import os
 
-def process_special(pdf_file):
+def process_special(pdf_file, start_rec=1):
     """
     Substitutes art in first page of the card with 
         I:\\Customer Files\\In Progress\\GO Monthly Web Orders\\
         32248 GO Dec 2018 Web Orders\\CARD_HolidayCard_MerryChristmas_FPCoverCX.pdf
+    Defaults as pdf with no header page
     """
     pdfFileObj = open(pdf_file, 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
@@ -25,7 +27,7 @@ def process_special(pdf_file):
 
     with open(os.path.join(os.curdir, 'split_replace', pdf_cards_file), 'wb') as save_cards:
         with open(os.path.join(os.curdir, 'split_replace', pdf_envelopes_file), 'wb') as save_envelopes:
-            for n, i in enumerate(all_pages, 1):
+            for n, i in enumerate(all_pages, start_rec):
                 if n < 1:
                     pass
                 else:
@@ -43,7 +45,8 @@ def process_special(pdf_file):
     pdfFileObj.close()
 
 
-def process(pdf_file):
+def process(pdf_file, start_rec=1):
+    """Defaults as pdf with no header page"""
     pdfFileObj = open(pdf_file, 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     all_pages = set(i for i in range(0, pdfReader.numPages))
@@ -58,7 +61,7 @@ def process(pdf_file):
 
     with open(os.path.join(os.curdir, 'split', pdf_cards_file), 'wb') as save_cards:
         with open(os.path.join(os.curdir, 'split', pdf_envelopes_file), 'wb') as save_envelopes:
-            for n, i in enumerate(all_pages, 1):
+            for n, i in enumerate(all_pages, start_rec):
                 if n < 1:
                     pass
                 else:
@@ -72,11 +75,27 @@ def process(pdf_file):
 
     pdfFileObj.close()
 
+
+def question():
+    ans = int(input("PDF contains cover page to skip (0 yes / 1 no): "))
+    if TypeError:
+        print("Error, invalid response")
+        time.sleep(3)
+        os.exit()
+
+    if ans not in {0, 1}:
+        print("Error, invalid response")
+        time.sleep(3)
+        os.exit()
+
+    return ans
+
 def main():
+    start_rec = question()
     pdf_list = [f for f in os.listdir() if str(f[-3:]).upper() == 'PDF']
     for pdf in pdf_list:
-        # process(pdf)
-        process_special(pdf)
+        # process(pdf, start_rec)
+        process_special(pdf, start_rec)
 
 if __name__ == '__main__':
     main()
